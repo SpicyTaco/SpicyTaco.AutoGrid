48e65246b7c6b600405f87368edd32f0dc6ff6b2
Kris McGinnes <kmcginnes@me.com>
2014-06-22 19:53:55 -0500

    Added gruntfile and package.json

---
+++ gruntfile.js
@@ -0,0 +1,29 @@
+module.exports = function(grunt) 
+{
+	grunt.loadNpmTasks('grunt-dotnet-assembly-info');
+    grunt.registerTask('default', []);
+    grunt.registerTask('ci', ['assemblyinfo']);
+
+    grunt.initConfig(
+    {
+    	assemblyinfo: 
+    	{
+            options: 
+            {
+                files: ['src/AutoGrid.sln'],
+                info: 
+                {
+                    title: 'Space Squirrel AutoGrid',
+		            description: 'Magical replacement for the default WPF Grid.', 
+		            configuration: 'Release', 
+		            company: 'Space Squirrel', 
+		            product: 'Space Squirrel AutoGrid', 
+		            copyright: 'Copyright 3002 © Space Squirrel', 
+		            trademark: 'Space Squirrel', 
+		            culture: 'div-MV',
+		            version: '0.1.0',
+		            fileVersion: '0.1.0'
+                }
+            }
+    });
+}
