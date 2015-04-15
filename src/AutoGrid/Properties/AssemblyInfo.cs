6a176987a306025e26626f864981d2115a2171d8
Kris McGinnes <kmcginnes@me.com>
2015-04-15 13:29:21 -0500

    Added xmlns schema url and default namespace shortcut

--- src/AutoGrid/Properties/AssemblyInfo.cs
+++ src/AutoGrid/Properties/AssemblyInfo.cs
@@ -1,6 +1,7 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Windows.Markup;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -34,3 +35,6 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("0.9.0")]
 [assembly: AssemblyVersion("0.9.0")]
 [assembly: AssemblyFileVersion("0.9.0")]
+
+[assembly: XmlnsPrefix("http://schemas.spicytaco.io/", "st")]
+[assembly: XmlnsDefinition("http://schemas.spicytaco.io/", "AutoGrid")]
