99305e2ec16b39ea82b9a370766510ed89ea538b
Kris McGinnes <kmcginnes@me.com>
2016-06-16 20:35:53 -0500

    Fixed height not responding to fill children

--- src/AutoGrid/StackPanel.cs
+++ src/AutoGrid/StackPanel.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -18,21 +16,11 @@ namespace AutoGrid
             double accumulatedWidth = 0;   // Total width consumed by children. 
             double accumulatedHeight = 0;   // Total height consumed by children. 
             
-            for (int i = 0, count = children.Count; i < count; ++i)
+            foreach (var child in children.OfType<UIElement>().Where(x => GetFill(x) == StackPanelFill.Auto))
             {
-                UIElement child = children[i];
                 Size childConstraint;             // Contains the suggested input constraint for this child.
                 Size childDesiredSize;            // Contains the return size from child measure. 
-
-                if (child == null) { continue; }
-
-                var fillType = GetFill(child);
-
-                if (fillType == StackPanelFill.Fill)
-                {
-                    continue;
-                }
-
+                
                 // Child constraint is the remaining size; this is total size minus size consumed by previous children.
                 childConstraint = new Size(Math.Max(0.0, constraint.Width - accumulatedWidth),
                                            Math.Max(0.0, constraint.Height - accumulatedHeight));
@@ -40,18 +28,7 @@ namespace AutoGrid
                 // Measure child.
                 child.Measure(childConstraint);
                 childDesiredSize = child.DesiredSize;
-
-                // Now, we adjust: 
-                // 1. Size consumed by children (accumulatedSize).  This will be used when computing subsequent
-                //    children to determine how much space is remaining for them. 
-                // 2. Parent size implied by this child (parentSize) when added to the current children (accumulatedSize).
-                //    This is different from the size above in one respect: A Dock.Left child implies a height, but does
-                //    not actually consume any height for subsequent children.
-                // If we accumulate size in a given dimension, the next child (or the end conditions after the child loop) 
-                // will deal with computing our minimum size (parentSize) due to that accumulation.
-                // Therefore, we only need to compute our minimum size (parentSize) in dimensions that this child does 
-                //   not accumulate: Width for Top/Bottom, Height for Left/Right. 
-
+                
                 if (Orientation == Orientation.Horizontal)
                 {
                     accumulatedWidth += childDesiredSize.Width;
@@ -76,11 +53,36 @@ namespace AutoGrid
                 accumulatedHeight += marginToAdd;
             }
 
+            foreach (var child in children.OfType<UIElement>().Where(x => GetFill(x) == StackPanelFill.Fill))
+            {
+                Size childConstraint;             // Contains the suggested input constraint for this child.
+                Size childDesiredSize;            // Contains the return size from child measure. 
+                
+                // Child constraint is the remaining size; this is total size minus size consumed by previous children.
+                childConstraint = new Size(Math.Max(0.0, constraint.Width - accumulatedWidth),
+                                           Math.Max(0.0, constraint.Height - accumulatedHeight));
+
+                // Measure child.
+                child.Measure(childConstraint);
+                childDesiredSize = child.DesiredSize;
+
+                if (Orientation == Orientation.Horizontal)
+                {
+                    accumulatedWidth += childDesiredSize.Width;
+                    accumulatedHeight = Math.Max(accumulatedHeight, childDesiredSize.Height);
+                }
+                else
+                {
+                    accumulatedWidth = Math.Max(accumulatedWidth, childDesiredSize.Width);
+                    accumulatedHeight += childDesiredSize.Height;
+                }
+            }
+
             // Make sure the final accumulated size is reflected in parentSize. 
             parentWidth = Math.Max(parentWidth, accumulatedWidth);
             parentHeight = Math.Max(parentHeight, accumulatedHeight);
 
-            return (new Size(parentWidth, parentHeight));
+            return new Size(parentWidth, parentHeight);
         }
 
         protected override Size ArrangeOverride(Size arrangeSize)
@@ -91,19 +93,29 @@ namespace AutoGrid
             double accumulatedLeft = 0;
             double accumulatedTop = 0;
 
-            var marginMultiplier = Math.Max(children.Count - 1, 0);
+            var marginMultiplier = Math.Max(totalChildrenCount - 1, 0);
             var totalMarginToAdd = MarginBetweenChildren * marginMultiplier;
             var isHorizontal = Orientation == Orientation.Horizontal;
 
-            var allAutoSizedSum = children.OfType<UIElement>()
-                .Where(x => GetFill(x) == StackPanelFill.Auto)
-                .Select(x => isHorizontal ? x.DesiredSize.Width : x.DesiredSize.Height)
-                .Sum();
+            double allAutoSizedSum = 0.0;
+            int countOfFillTypes = 0;
+            foreach (var child in children.OfType<UIElement>())
+            {
+                var fillType = GetFill(child);
+                if (fillType == StackPanelFill.Fill)
+                {
+                    countOfFillTypes += 1;
+                }
+                else
+                {
+                    var desiredSize = isHorizontal ? child.DesiredSize.Width : child.DesiredSize.Height;
+                    allAutoSizedSum += desiredSize;
+                }
+            }
+
             var remainingForFillTypes = isHorizontal
                 ? Math.Max(0, arrangeSize.Width - allAutoSizedSum - totalMarginToAdd)
                 : Math.Max(0, arrangeSize.Height - allAutoSizedSum - totalMarginToAdd);
-            var countOfFillTypes = children.OfType<UIElement>()
-                .Count(x => GetFill(x) == StackPanelFill.Fill);
             var fillTypeSize = remainingForFillTypes / countOfFillTypes;
 
             for (int i = 0; i < totalChildrenCount; ++i)
